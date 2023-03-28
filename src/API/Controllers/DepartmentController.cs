b955d4c19788c06aa845fc64659137a06d57d830
abayomi igwubor <unbeatable-abayomi>
2023-03-28 12:06:57 +0100

    added gitignore

---
+++ src/API/Controllers/DepartmentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+     [ApiController]
+     [Route("[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        // GET
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("get all students");
+        }
+
+
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get this " + code + " department data" );
+        }
+        
+        
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("insert new department");
+        }
+        
+        [HttpPut("{code}")]
+        public IActionResult Update(string code)
+        {
+            return Ok("update this " + code + " department data" );
+        }
+        
+         
+        
+        [HttpDelete("{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("delete this " + code + " department data" );
+        }
+    }
+}
